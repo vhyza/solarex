78d09f13372387b056d2f55cc7b9849b37d2a0e0
Vojtech Hyza <vhyza@vhyza.eu>
2019-04-16 17:19:55 +0200

    Initial commit
    * generated by Mix

---
+++ lib/solarex.ex
@@ -0,0 +1,18 @@
+defmodule Solarex do
+  @moduledoc """
+  Documentation for Solarex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Solarex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/solarex_test.exs
@@ -0,0 +1,8 @@
+defmodule SolarexTest do
+  use ExUnit.Case
+  doctest Solarex
+
+  test "greets the world" do
+    assert Solarex.hello() == :world
+  end
+end
